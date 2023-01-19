b7b9533e79598a2af5307ff523827b571d705dd2
Abenx <5195693+rushairer@users.noreply.github.com>
2023-01-19 11:40:24 +0800

    fix: available

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         .macCatalyst(.v16),
         .watchOS(.v9),
         .macOS(.v13),
+        .tvOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
